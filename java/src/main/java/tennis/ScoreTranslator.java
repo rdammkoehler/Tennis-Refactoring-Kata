9ff54251a0a3300831721507722505f5698c1c4d
Rich Dammkoehler <rpd@noradltd.com>
2015-07-07 19:02:51 -0400

    removed use of .ordinal for tie score compares

--- java/src/main/java/tennis/ScoreTranslator.java
+++ java/src/main/java/tennis/ScoreTranslator.java
@@ -11,26 +11,28 @@ public class ScoreTranslator {
 	public String translate(Points player1Score, Points player2Score) {
 		String score = "";
 		if (player1Score == player2Score) {
-			if (player1Score.ordinal() > THIRTY.ordinal()) {
+			if (THIRTY.compareTo(player1Score) < 0) {
 				score = "Deuce";
 			} else {
 				score = player1Score + "-All";
 			}
 		} else if (player1Score.ordinal() > FORTY.ordinal() || player2Score.ordinal() > FORTY.ordinal()) {
-			Map<Integer,String> scores = new HashMap<Integer,String>() {
+			Map<Integer, String> scores = new HashMap<Integer, String>() {
 				private static final long serialVersionUID = 1L;
+
 				{
 					put(-1, "Advantage player2");
-					put(0,"impossible");
+					put(0, "impossible");
 					put(1, "Advantage player1");
 				}
+
 				@Override
 				public String get(Object offset) {
 					String score;
-					if ( containsKey(offset)) {
+					if (containsKey(offset)) {
 						score = super.get(offset);
 					} else {
-						if ( ((Integer)offset).intValue() < 0 ) {
+						if (((Integer) offset).intValue() < 0) {
 							score = "Win for player2";
 						} else {
 							score = "Win for player1";

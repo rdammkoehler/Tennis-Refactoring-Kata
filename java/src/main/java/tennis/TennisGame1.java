2e81cba3cd214577bcdfbab8411effdad81c63d6
Rich Dammkoehler <rpd@noradltd.com>
2015-07-07 17:05:24 -0400

    removed 0xdeadcode from TennisGame1 until needed by tests

--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -4,8 +4,6 @@ public class TennisGame1 implements TennisGame {
 
 	private int m_score1 = 0;
 	private int m_score2 = 0;
-	private String player1Name;
-	private String player2Name;
 
 	public TennisGame1(String player1Name, String player2Name) {
 		if ( player1Name == null ) {
@@ -17,8 +15,6 @@ public class TennisGame1 implements TennisGame {
 		if ( player1Name.equals(player2Name) ) {
 			throw new IllegalArgumentException("Player names must be unique");
 		}
-		this.player1Name = player1Name;
-		this.player2Name = player2Name;
 	}
 
 	public void wonPoint(String playerName) {

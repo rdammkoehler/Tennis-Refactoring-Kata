82f2a579e69265f33292051f5773b395eb3c16e3
Rich Dammkoehler <rpd@noradltd.com>
2015-07-07 19:13:53 -0400

    refactored out actions to digestable methods

--- java/src/main/java/tennis/ScoreTranslator.java
+++ java/src/main/java/tennis/ScoreTranslator.java
@@ -11,40 +11,61 @@ public class ScoreTranslator {
 	public String translate(Points player1Score, Points player2Score) {
 		String score = "";
 		if (player1Score == player2Score) {
-			if (player1Score.greaterThan(THIRTY)) {
-				score = "Deuce";
-			} else {
-				score = player1Score + "-All";
-			}
+			score = getTieScore(player1Score);
 		} else if (player1Score.greaterThan(FORTY) || player2Score.greaterThan(FORTY)) {
-			Map<Integer, String> scores = new HashMap<Integer, String>() {
-				private static final long serialVersionUID = 1L;
-
-				{
-					put(-1, "Advantage player2");
-					put(0, "impossible");
-					put(1, "Advantage player1");
-				}
-
-				@Override
-				public String get(Object offset) {
-					String score;
-					if (containsKey(offset)) {
-						score = super.get(offset);
-					} else {
-						if (((Integer) offset).intValue() < 0) {
-							score = "Win for player2";
-						} else {
-							score = "Win for player1";
-						}
-					}
-					return score;
-				}
-			};
-			int scoreOffset = player1Score.intValue() - player2Score.intValue();
-			score = scores.get(scoreOffset);
+			score = getWinningOrAdvantageScore(player1Score, player2Score);
 		} else {
-			score = player1Score + "-" + player2Score;
+			score = getInGameScore(player1Score, player2Score);
+		}
+		return score;
+	}
+
+	private String getWinningOrAdvantageScore(Points player1Score, Points player2Score) {
+		String score;
+		Map<Integer, String> scores = new HashMap<Integer, String>() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				put(-1, "Advantage player2");
+				put(0, "impossible");
+				put(1, "Advantage player1");
+			}
+
+			@Override
+			public String get(Object offset) {
+				String score;
+				if (containsKey(offset)) {
+					score = super.get(offset);
+				} else {
+					if (((Integer) offset).intValue() < 0) {
+						score = "Win for player2";
+					} else {
+						score = "Win for player1";
+					}
+				}
+				return score;
+			}
+		};
+		score = scores.get(getScoreOffset(player1Score, player2Score));
+		return score;
+	}
+
+	private int getScoreOffset(Points player1Score, Points player2Score) {
+		return player1Score.intValue() - player2Score.intValue();
+	}
+
+	private String getInGameScore(Points player1Score, Points player2Score) {
+		String score;
+		score = player1Score + "-" + player2Score;
+		return score;
+	}
+
+	private String getTieScore(Points player1Score) {
+		String score;
+		if (player1Score.greaterThan(THIRTY)) {
+			score = "Deuce";
+		} else {
+			score = player1Score + "-All";
 		}
 		return score;
 	}

993706fd806e471a21cb94f685bd686a342fe900
Rich Dammkoehler <rpd@noradltd.com>
2015-07-07 17:04:40 -0400

    test code refactor, extracted constant for player name

--- java/src/test/java/tennis/TennisGame1Test.java
+++ java/src/test/java/tennis/TennisGame1Test.java
@@ -4,18 +4,20 @@ import org.junit.Test;
 
 public class TennisGame1Test {
 
+	private static final String PLAYER_NAME = "Andre Agassi";
+
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldRejectSinglePlayerGames() {
-		new TennisGame1("Andre Agassi", "Andre Agassi");
+		new TennisGame1(PLAYER_NAME, PLAYER_NAME);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldRejectNullPlayerNameOne() {
-		new TennisGame1(null, "Andre Agassi");
+		new TennisGame1(null, PLAYER_NAME);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldRejectNullPlayerNameTwo() {
-		new TennisGame1("Andre Agassi", null);
+		new TennisGame1(PLAYER_NAME, null);
 	}
 }

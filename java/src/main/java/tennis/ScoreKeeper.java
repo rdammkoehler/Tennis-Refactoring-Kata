88bad4b31e6f61dc5dd84ed8d8ba22b3badd5870
Rich Dammkoehler <rpd@noradltd.com>
2015-07-07 17:46:57 -0400

    isolated score keeping into its own class

---
+++ java/src/main/java/tennis/ScoreKeeper.java
@@ -0,0 +1,43 @@
+package tennis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreKeeper {
+
+	private Map<String, Integer> scores = new HashMap<String, Integer>();
+	private String player1, player2;
+
+	public ScoreKeeper(String player1Name, String player2Name) {
+		if (player1Name == null) {
+			throw new IllegalArgumentException("Player One must have a proper name");
+		}
+		if (player2Name == null) {
+			throw new IllegalArgumentException("Player Two must have a proper name");
+		}
+		if (player1Name.equals(player2Name)) {
+			throw new IllegalArgumentException("Player names must be unique");
+		}
+		player1 = player1Name;
+		scores.put(player1Name, 0);
+		player2 = player2Name;
+		scores.put(player2Name, 0);
+	}
+
+	public void incrementScoreOf(String playerName) {
+		if (scores.containsKey(playerName)) {
+			scores.put(playerName, scores.get(playerName) + 1);
+		} else {
+			throw new IllegalArgumentException(playerName + " is not part of this game");
+		}
+	}
+
+	public int scoreOfPlayer1() {
+		return scores.get(player1);
+	}
+
+	public int scoreOfPlayer2() {
+		return scores.get(player2);
+	}
+
+}
--- java/src/main/java/tennis/TennisGame1.java
+++ java/src/main/java/tennis/TennisGame1.java
@@ -1,38 +1,18 @@
 package tennis;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class TennisGame1 implements TennisGame {
 
-	private Map<String, Integer> scores = new HashMap<String, Integer>();
-	private String player1, player2;
+	private ScoreKeeper scoreKeeper;
 
 	public TennisGame1(String player1Name, String player2Name) {
-		if (player1Name == null) {
-			throw new IllegalArgumentException("Player One must have a proper name");
-		}
-		if (player2Name == null) {
-			throw new IllegalArgumentException("Player Two must have a proper name");
-		}
-		if (player1Name.equals(player2Name)) {
-			throw new IllegalArgumentException("Player names must be unique");
-		}
-		player1 = player1Name;
-		scores.put(player1Name, 0);
-		player2 = player2Name;
-		scores.put(player2Name, 0);
+		scoreKeeper = new ScoreKeeper(player1Name, player2Name);
 	}
 
 	public void wonPoint(String playerName) {
-		if (scores.containsKey(playerName)) {
-			scores.put(playerName, scores.get(playerName) + 1);
-		} else {
-			throw new IllegalArgumentException(playerName + " is not part of this game");
-		}
+		scoreKeeper.incrementScoreOf(playerName);
 	}
 
 	public String getScore() {
-		return new ScoreTranslator().translate(scores.get(player1), scores.get(player2));
+		return new ScoreTranslator().translate(scoreKeeper.scoreOfPlayer1(), scoreKeeper.scoreOfPlayer2());
 	}
 }

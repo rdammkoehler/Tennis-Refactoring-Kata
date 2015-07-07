8d113db8b773913dbe1b1122250de187a63c94a2
Rich Dammkoehler <rpd@noradltd.com>
2015-07-07 19:48:54 -0400

    test code cleanups

--- java/src/test/java/tennis/TennisGame1Test.java
+++ java/src/test/java/tennis/TennisGame1Test.java
@@ -7,40 +7,40 @@ import org.junit.Test;
 
 public class TennisGame1Test {
 
-	private static final String PLAYER_MCENRO = "John McEnro";
-	private static final String PLAYER_AGASSI = "Andre Agassi";
-	private TennisGame game = new TennisGame1(PLAYER_AGASSI, PLAYER_MCENRO);
+	private static final String PLAYER1 = "Andre Agassi";
+	private static final String PLAYER2 = "John McEnro";
+	private TennisGame game = new TennisGame1(PLAYER1, PLAYER2);
 
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldRejectSinglePlayerGames() {
-		new TennisGame1(PLAYER_AGASSI, PLAYER_AGASSI);
+		new TennisGame1(PLAYER1, PLAYER1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldRejectNullPlayerNameOne() {
-		new TennisGame1(null, PLAYER_AGASSI);
+		new TennisGame1(null, PLAYER1);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
 	public void shouldRejectNullPlayerNameTwo() {
-		new TennisGame1(PLAYER_AGASSI, null);
+		new TennisGame1(PLAYER1, null);
 	}
 
 	@Test
 	public void playerOneAndPlayerTwoCanBeDistinguished() {
-		game.wonPoint(PLAYER_AGASSI);
-		game.wonPoint(PLAYER_AGASSI);
-		game.wonPoint(PLAYER_AGASSI);
-		game.wonPoint(PLAYER_AGASSI);
+		game.wonPoint(PLAYER1);
+		game.wonPoint(PLAYER1);
+		game.wonPoint(PLAYER1);
+		game.wonPoint(PLAYER1);
 		assertThat(game.getScore(), is("Win for player1"));
 	}
 
 	@Test
 	public void playerTwoAndPlayerOneCanBeDistinguished() {
-		game.wonPoint(PLAYER_MCENRO);
-		game.wonPoint(PLAYER_MCENRO);
-		game.wonPoint(PLAYER_MCENRO);
-		game.wonPoint(PLAYER_MCENRO);
+		game.wonPoint(PLAYER2);
+		game.wonPoint(PLAYER2);
+		game.wonPoint(PLAYER2);
+		game.wonPoint(PLAYER2);
 		assertThat(game.getScore(), is("Win for player2"));
 	}
 

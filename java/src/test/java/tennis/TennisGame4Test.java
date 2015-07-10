3034ebd8e10feee9a9a8f02c95616fdd28115946
Rich Dammkoehler <rpd@noradltd.com>
2015-07-10 12:34:33 -0400

    tiny refactor to cleanup duplicate string literal

--- java/src/test/java/tennis/TennisGame4Test.java
+++ java/src/test/java/tennis/TennisGame4Test.java
@@ -7,10 +7,13 @@ import org.junit.Test;
 
 public class TennisGame4Test {
 
+	private static final String PLAYERNAME_ANDRE_AGASSI = "Andre Agassi";
+
 	@Test
 	public void wonPointAddsPointToPlayerByName() {
 		TennisGame4 game = new TennisGame4();
-		game.wonPoint("Andre Agassi");
-		assertThat(game.getCurrentPoints("Andre Agassi").toString(), is("Fifteen"));
+		game.wonPoint(PLAYERNAME_ANDRE_AGASSI);
+		assertThat(game.getCurrentPoints(PLAYERNAME_ANDRE_AGASSI).toString(), is("Fifteen"));
 	}
+	
 }

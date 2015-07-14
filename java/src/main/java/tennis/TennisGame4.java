a7563cce38b9bebde1e5ed5b15cd7fe627868bb2
Rich Dammkoehler <rpd@noradltd.com>
2015-07-14 17:23:31 -0400

    simplify the score tracking into just maintaining some ints

--- java/src/main/java/tennis/TennisGame4.java
+++ java/src/main/java/tennis/TennisGame4.java
@@ -29,39 +29,41 @@ public class TennisGame4 implements TennisGame {
 		}
 	}
 
-	private static final Map<Integer,Map<Integer,String>> SCORE_TABLE = new HashMap<Integer,Map<Integer,String>>() {
+	private static final Map<Integer, Map<Integer, String>> SCORE_TABLE = new HashMap<Integer, Map<Integer, String>>() {
 		private static final long serialVersionUID = 1L;
+
 		{
-			String[][] basic_scores = { 
-					{ null, null, null, null, "Love-All", null, null, null, null, },
+			String[][] basic_scores = { { null, null, null, null, "Love-All", null, null, null, null, },
 					{ null, null, null, "Love-Fifteen", null, "Fifteen-Love", null, null, null, },
 					{ null, null, "Love-Thirty", null, "Fifteen-All", null, "Thirty-Love", null, null, },
 					{ null, "Love-Forty", null, "Fifteen-Thirty", null, "Thirty-Fifteen", null, "Forty-Love", null, },
-					{ "Win for player2", null, "Fifteen-Forty", null, "Thirty-All", null, "Forty-Fifteen", null, "Win for player1" },
-					{ null, "Win for player2", null, "Thirty-Forty", null, "Forty-Thirty", null, "Win for player1", null, },
+					{ "Win for player2", null, "Fifteen-Forty", null, "Thirty-All", null, "Forty-Fifteen", null,
+							"Win for player1" },
+					{ null, "Win for player2", null, "Thirty-Forty", null, "Forty-Thirty", null, "Win for player1",
+							null, },
 					{ null, null, "Win for player2", null, "Deuce", null, "Win for player1", null, null, },
-					{ null, null, null, "Advantage player2", "Deuce", "Advantage player1", null, null, null, },
-			};
-			for(int sum=0; sum<8;sum++) {
-				this.put(sum, new HashMap<Integer,String>());
-				for(int idx=0;idx<basic_scores[sum].length;idx++) {
-					this.get(sum).put(idx-4, basic_scores[sum][idx]);
+					{ null, null, null, "Advantage player2", "Deuce", "Advantage player1", null, null, null, }, };
+			for (int sum = 0; sum < 8; sum++) {
+				this.put(sum, new HashMap<Integer, String>());
+				for (int idx = 0; idx < basic_scores[sum].length; idx++) {
+					this.get(sum).put(idx - 4, basic_scores[sum][idx]);
 				}
 			}
 		}
 	};
-	
-	private static final String[] SCORES_BEYOND_DEUCE =  { "Win for player2", "Advantage player2", "Deuce", "Advantage player1", "Win for player1" };
-	
-	private Map<String, Integer> playerNameToScoreStringMap = new HashMap<String, Integer>();
-	private String player1Key, player2Key;
+
+	private static final String[] SCORES_BEYOND_DEUCE = { "Win for player2", "Advantage player2", "Deuce",
+			"Advantage player1", "Win for player1" };
+
+	private int player1score, player2score;
+	private String player1Name, player2Name;
 
 	public TennisGame4(String player1, String player2) {
 		validate(player1, player2);
-		playerNameToScoreStringMap.put(player1, 0);
-		playerNameToScoreStringMap.put(player2, 0);
-		player1Key = player1;
-		player2Key = player2;
+		player1Name = player1;
+		player1score = 0;
+		player2Name = player2;
+		player2score = 0;
 	}
 
 	private void validate(String player1, String player2) {
@@ -74,22 +76,21 @@ public class TennisGame4 implements TennisGame {
 	}
 
 	public void wonPoint(String playerName) {
-		if (!playerNameToScoreStringMap.containsKey(playerName)) {
-			throw new ScoringException("Only two players may score in the same game");
+		if (playerName.equals(player1Name)) {
+			player1score++;
+		} else if (playerName.equals(player2Name)) {
+			player2score++;
 		} else {
-			int currentScoreStringIndex = playerNameToScoreStringMap.get(playerName);
-			int newScoreStringIndex = currentScoreStringIndex + 1;
-			playerNameToScoreStringMap.put(playerName, newScoreStringIndex);
+			throw new ScoringException("Only two players may score in the same game");
 		}
 	}
 
-	
 	public String getScore() {
 		String score;
 		int sum = getSumOfPlayerScores();
 		int diff = getDifferenceOfPlayerScores();
-		if ( sum > 7 ) {
-			score = SCORES_BEYOND_DEUCE[diff+2];
+		if (sum > 7) {
+			score = SCORES_BEYOND_DEUCE[diff + 2];
 		} else {
 			score = SCORE_TABLE.get(sum).get(diff);
 		}
@@ -97,12 +98,12 @@ public class TennisGame4 implements TennisGame {
 	}
 
 	private int getDifferenceOfPlayerScores() {
-		int diff = playerNameToScoreStringMap.get(player1Key)-playerNameToScoreStringMap.get(player2Key);
+		int diff = player1score - player2score;
 		return diff;
 	}
 
 	private int getSumOfPlayerScores() {
-		int sum = playerNameToScoreStringMap.get(player1Key)+playerNameToScoreStringMap.get(player2Key);
+		int sum = player1score + player2score;
 		return sum;
 	}
 }
